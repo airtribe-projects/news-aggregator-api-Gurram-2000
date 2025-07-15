de7e82b9aa3a7cf646e35adc21890ae702e39c6e
gurramyaswanth <gvbsai2000@gmail.com>
2025-07-15 13:16:21 +0530

    Commiting the chnages to feedback brnach-Total project

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+// Route handling
+const authRoutes = require('./apiRoutes/apiroutes');
+app.use('/', authRoutes);
+
+// Server listener
+app.listen(port, (error) => {
+  if (error) {
+    console.error('Server failed to start:', error);
+    return;
+  }
+  console.log(`Server is running and listening on port ${port}`);
 });
-
-
 
 module.exports = app;
